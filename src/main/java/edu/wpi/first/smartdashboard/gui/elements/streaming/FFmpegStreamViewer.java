aea71abc88c718d6bde8d2fa0ca4da5ab449782a
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-01-24 20:10:51 -0500

    Disable buffering

--- src/main/java/edu/wpi/first/smartdashboard/gui/elements/streaming/FFmpegStreamViewer.java
+++ src/main/java/edu/wpi/first/smartdashboard/gui/elements/streaming/FFmpegStreamViewer.java
@@ -46,7 +46,7 @@ public class FFmpegStreamViewer extends StreamViewer {
       while (!interrupted()) {
         try {
           grabber = new FFmpegFrameGrabber(url);
-          grabber.setOption("protocol_whitelist", "file,crypto,rtp,udp");
+          grabber.setOption("fflags", "nobuffer");
           grabber.start();
           Java2DFrameConverter converter = new Java2DFrameConverter();
           while (!interrupted() && !isCameraChanged()) {
