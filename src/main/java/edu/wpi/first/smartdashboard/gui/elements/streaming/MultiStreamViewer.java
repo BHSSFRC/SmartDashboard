d5d9aff07a362cc82241403cd91d705e059bade7
Caleb Berger <caleb.x.berger@gmail.com>
2019-03-14 20:08:34 -0400

    Change stream in MultiStreamViewer on POV tilt

--- src/main/java/edu/wpi/first/smartdashboard/gui/elements/streaming/MultiStreamViewer.java
+++ src/main/java/edu/wpi/first/smartdashboard/gui/elements/streaming/MultiStreamViewer.java
@@ -1,29 +1,33 @@
 package edu.wpi.first.smartdashboard.gui.elements.streaming;
 
-import edu.wpi.first.smartdashboard.properties.MultiProperty;
-import edu.wpi.first.smartdashboard.properties.Property;
+import edu.wpi.first.smartdashboard.joystick.JInputJoystick;
 import edu.wpi.first.smartdashboard.properties.StringProperty;
 
+import java.awt.*;
+
 public class MultiStreamViewer extends FFmpegStreamViewer {
-  final StringProperty streamOneURL = new StringProperty(this, "Stream One URL", "");
-  final StringProperty streamTwoURL = new StringProperty(this, "Stream Two URL", "");
-  final MultiProperty selectedStream = new MultiProperty(this, "Selected stream");
+  private final StringProperty streamOneURL = new StringProperty(this, "Stream One URL", "");
+  private final StringProperty streamTwoURL = new StringProperty(this, "Stream Two URL", "");
+  private JInputJoystick joystick;
 
   public MultiStreamViewer() {
     super();
-    selectedStream.add("Stream one", streamOneURL);
-    selectedStream.add("Stream two", streamTwoURL);
+    this.joystick = new JInputJoystick(0);
   }
 
   @Override
-  public void onPropertyChanged(Property property) {
-    super.onPropertyChanged(property);
-    if (property.equals(selectedStream)) {
-      // dangerous
-      url = ((StringProperty) selectedStream.getValue()).getValue();
-      // awful
-      urlProperty.setValue(url);
-      cameraChanged();
+  protected final void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    // HACK: paintComponent is the closest thing we have to a "main loop" without redoing the whole dashboard
+    // joy input goes here
+    this.joystick.pollController();
+    float pov = this.joystick.getHatSwitchPosition();
+    if (Float.compare(pov, 0.75f) == 0) {
+      urlProperty.setValue(streamTwoURL.getValue());
+      propertyChanged(urlProperty);
+    } else if (Float.compare(pov, 0.25f) == 0) {
+      urlProperty.setValue(streamOneURL.getValue());
+      propertyChanged(urlProperty);
     }
   }
 }
